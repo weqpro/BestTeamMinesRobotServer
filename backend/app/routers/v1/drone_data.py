8fddef17e77277e4d40c6fa17909d5627804e3ce
weqpro <weqpro@gmail.com>
2025-05-31 12:02:42 +0300

    detect_metal

--- backend/app/routers/v1/drone_data.py
+++ backend/app/routers/v1/drone_data.py
@@ -5,3 +5,8 @@ from app.repository import UpdateRepository, get_update_repository
 from app.schemas import UpdateSchema, UpdateCreateSchema
 
 router = APIRouter(prefix="/drone_data")
+
+
+@router.post("/detect_metal")
+async def detect_metal():
+    print("METAL DETECTED!")
--- backend/app/schemas/drone_data.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class DroneData(BaseModel):
-    ax: int
-    ay: int
-    az: int
-    distance: float
-    metal_detect: float
